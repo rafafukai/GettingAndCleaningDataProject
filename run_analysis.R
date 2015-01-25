4de19586c2447db55519ae62ffbf333ab57a2c1a
rafa <rafael.fukai@gmail.com>
2015-01-25 10:21:32 -0500

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+## packages
+library(data.table)
+library(dplyr)
+
 
 ## You should create one R script called run_analysis.R that does the following. 
 ## 1. Merges the training and the test sets to create one data set.
