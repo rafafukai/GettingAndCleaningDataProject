c5503b4f9d044211d32b0f868a326bb04e408fdd
rafa <rafael.fukai@gmail.com>
2015-01-25 11:31:45 -0500

    download step

--- run_analysis.R
+++ run_analysis.R
@@ -20,6 +20,12 @@ library(dplyr)
     ## download data
     download.file(url,destfile = dfile)
 
+    ## unzip data (list files)
+    ##unzip(dfile, list = TRUE)
+    
+    ## unlink
+    unlink(temp)
+
 
 ## You should create one R script called run_analysis.R that does the following. 
 ## 1. Merges the training and the test sets to create one data set.
