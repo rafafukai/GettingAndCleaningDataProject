2b2d615f273614b5b065dd14ab011dc76344b716
rafa <rafael.fukai@gmail.com>
2015-01-25 12:17:26 -0500

    assignment 2

--- run_analysis.R
+++ run_analysis.R
@@ -39,7 +39,6 @@ library(dplyr)
     ## features 'feat' table lists columns in test (561)
     colnames(x_test)  <- t(feat[2])
     colnames(x_train) <- t(feat[2])
-    
 
 
 ## -----------------------------------------------------------------------------------------------------
@@ -53,8 +52,28 @@ library(dplyr)
     x_merge <- x_merge[, !duplicated(colnames(x_merge))]
 
 
-
+## -----------------------------------------------------------------------------------------------------
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+    ## mean:
+    ## get columns
+    x_mean <- grep("mean()", names(x_merge), value = FALSE, fixed = TRUE)
+      x_mean <- append(x_mean, grep("gravityMean", names(x_merge), value = FALSE, fixed = TRUE))
+      x_mean <- append(x_mean, grep("tBodyAccMean", names(x_merge), value = FALSE, fixed = TRUE))
+      x_mean <- append(x_mean, grep("tBodyAccJerkMean", names(x_merge), value = FALSE, fixed = TRUE))
+      x_mean <- append(x_mean, grep("tBodyGyroMean", names(x_merge), value = FALSE, fixed = TRUE))
+      x_mean <- append(x_mean, grep("tBodyGyroJerkMean", names(x_merge), value = FALSE, fixed = TRUE))
+    ## extract meas
+    meas_xMean <- x_merge[x_mean]
+
+    ## std:
+    ## get columns
+    x_std <- grep("std()", names(x_merge), value = FALSE, fixed = TRUE)
+    ## extract meas
+    meas_xSTD <- x_merge[x_std]
+
+
+## -----------------------------------------------------------------------------------------------------
 ## 3. Uses descriptive activity names to name the activities in the data set
 ## 4. Appropriately labels the data set with descriptive variable names. 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
